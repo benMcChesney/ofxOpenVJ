769014497c6fbd12e71a503d87aa32d68b40faa8
Ben McChesney <ben.mcchesney@gmail.com>
2013-04-24 20:24:58 -0700

    Included addons script

---
+++ clone_addons.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Dependencies for ofxOpenVJ
+# inspired by ObviousJim and ofxTimeline script
+
+
+cd ../
+
+git clone https://github.com/HeliosInteractive/ofxSimpleMask.git
+
+git clone https://github.com/kylemcdonald/ofxFft.git
+
+git clone https://github.com/ofTheo/ofxKinect.git
+
+git clone https://github.com/NickHardeman/ofxTweenzor.git
+
+git clone https://github.com/rezaali/ofxUI.git
+
+git clone https://github.com/timscaffidi/ofxParticles.git
